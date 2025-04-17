a0c2a8666eab9a4324012e2af320f0433176970d
licheng0794 <lichengxlxl@gmail.com>
2025-04-18 08:54:07 +1000

    Update model.py

--- model.py
+++ model.py
@@ -17,7 +17,7 @@ def plot_predictions(train_data, train_labels,  test_data, test_labels,  predict
   # Plot the predictions in red (predictions were made on the test data)
   plt.scatter(test_data, predictions, c="r", label="Predictions")
   # Show the legend
-  plt.legend(shadow='True')
+  plt.legend(shadow=True)
   # Set grids
   plt.grid(which='major', c='#cccccc', linestyle='--', alpha=0.5)
   # Some text
